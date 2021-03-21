26918b4dcb8738d817ef00b07b51af0ce8e066af
Aapuji <om.robotics16@gmail.com>
2021-03-21 14:10:37 -0400

    Added a scripts, but it doesn't work, most likely due to the remote link in index not working.  BUt I am not bothering to chn=ange it now.

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
-console.log('Delete This');
+/*const highlightCurrentLink = () => {
+    for (let i = 1; i <= $('ul.highlightCurrentLink li').length; i++) {
+        let listItem = $(`ul.highlightCurrentLink li:nth-child(${i})`);
+        if (listItem === location.href) {
+            listItem.css('color', 'var(--current-url-color)');
+        }
+    }
+}  --JQUERY  
+
+Its supposed to highlight the current url's link.   I think it is because the remote link doesn't work, but I don't want to download anything to the folder.
+
+Doesn't work*/
