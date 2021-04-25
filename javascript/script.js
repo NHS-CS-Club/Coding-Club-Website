4ad9cdfe44ae23b5f7c83fad13d4967ed391b1bd
Aapuji <om.robotics16@gmail.com>
2021-04-25 11:44:09 -0400

    Got rid of the useless piece of code

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,1 @@
-const highlightCurrentLink = () => {
-    for (let i = 1; i <= $('ul.highlightCurrentLink li').length; i++) {
-        let listItem = $(`ul.highlightCurrentLink li:nth-child(${i})`);
-        if (listItem === location.href) {
-            listItem.css('color', 'var(--current-url-color)');
-        }
-    }
-    console.log('Actually it does!');
-}  /*--JQUERY  
-
-Its supposed to highlight the current url's link.   I think it is because the remote link doesn't work, but I don't want to download anything to the folder.
-
-Doesn't work*/
-console.log('Hi, this doesn\'t work');
+// script.js
