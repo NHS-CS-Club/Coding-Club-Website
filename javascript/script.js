4e0b0556cae8a57bbe9866f9dbb497dc943fd33d
Aapuji <om.robotics16@gmail.com>
2021-04-25 11:46:37 -0400

    Added the jQuery checker in case if we want to use it.

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,11 @@
-// script.js
+// Check jQuery
+window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        console.log('Working');
+    } else {
+        console.log('Not yet');
+     location.reload();
+    }
+ }
+ 
